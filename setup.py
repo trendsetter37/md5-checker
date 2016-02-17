e21c7c0b87aa1b15a0bd64c4a09d13fd4ad6b617
Javis Sullivan <javissullivan@gmail.com>
2016-02-17 17:20:54 -0500

    setup test env

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # Modules
     py_modules=['checkmd5'],
