11067fcce8c6d5303f683b53a1ba2a7fd7c20b65
Javis Sullivan <javissullivan@gmail.com>
2016-02-22 17:17:31 -0500

    add multiple algorimth functionality

--- md5checker/__init__.py
+++ md5checker/__init__.py
@@ -3,11 +3,21 @@ from . import checkmd5 as c
 import sys
 import os
 make_hash = c.make_hash
+_FLAGS = ('-a', '--algo')
 
 def main():
     ''' Entry point for the application script '''
     args = sys.argv
-    if len(args) != 2:
-        print('Usage: md5checker "path-to-file"')
-    else:
-        print(make_hash(sys.argv[1]))
+    length = len(args)
+
+    if length < 2 or 4 < length <= 3:
+        print('Usage: md5checker "path-to-file" [options]')
+    elif length == 2:
+        print(make_hash(args[1]))
+    elif length == 4:
+        flag = args[2]
+        if flag in _FLAGS:
+            print(make_hash(args[1], algo=args[3]))
+        else:
+            print('Optional flags are -a or --algo')
+            sys.exit(1)
--- md5checker/checkmd5.py
+++ md5checker/checkmd5.py
@@ -1,14 +1,33 @@
-''' Module contains functions required for hashing file data '''
+''' Module contains functions required for hashing file data
+
+    Guaranteed algorithms are:
+    * md5
+    * sha1
+    * sha224
+    * sha256
+    * sha384
+    * sha512
+ '''
 from __future__ import print_function
 import os
 import hashlib
 
-def make_hash(filename):
+def make_hash(filename, algo='md5'):
     '''
         Returns an md5 hash of the data in the file.
         For use as an imported imported function
+
+        algo keyword argument should be one of the following:
+
+        * md5
+        * sha1
+        * sha224
+        * sha256
+        * sha384
+        * sha512
+
     '''
-    m = hashlib.md5()
+    m = getattr(hashlib, algo)()
     try:
         path = os.path.abspath(filename)
         with open(path, 'rb') as fname:
