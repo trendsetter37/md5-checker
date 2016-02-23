318fc4935dff575cc56d4dc953cdacd8d6983ba1
Javis Sullivan <javissullivan@gmail.com>
2016-02-23 12:03:07 -0500

    clarify fixture usage in test modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,6 @@ DATA_PATH = os.path.abspath(_join(DIRECTORY, 'data'))
 
 @pytest.fixture(scope='module')
 def files():
+    ''' Available to other test modules as files object '''
     with open(str(_join(DATA_PATH, 'data.json')), 'r') as f:
         return json.loads(f.read())
