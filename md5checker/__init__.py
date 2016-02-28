2fe439781b21f9a953f517d12b6f54375847e559
Javis Sullivan <javissullivan@gmail.com>
2016-02-28 15:28:11 -0500

    update version

--- md5checker/__init__.py
+++ md5checker/__init__.py
@@ -5,7 +5,7 @@ import os
 import hashlib
 make_hash = c.make_hash
 _FLAGS = ('-a', '--algo')
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 def usage():
     print('Usage: md5checker "path-to-file" [options]')
